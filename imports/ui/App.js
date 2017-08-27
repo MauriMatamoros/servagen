86e6aea0b54502ef63a2af5d0df1b977fa17cb78
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2017-08-26 23:58:47 -0600

    Redoing AddClients File

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import NavBar from './NavBar';
+import AddClients from './AddClients';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <NavBar/>
+        <AddClients/>
       </div>
     );
   }
