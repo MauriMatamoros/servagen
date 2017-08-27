85f912bebab7f0213c7416ba50ca64274791dacb
MauriMatamoros <mauricio.enrique.matamoros@gmail.com>
2017-08-27 14:42:15 -0600

    Resizing Form

--- imports/ui/AddClients.js
+++ imports/ui/AddClients.js
@@ -30,40 +30,46 @@ export default class AddClients extends React.Component {
   };
   render() {
     return(
-      <form onSubmit={this.onSubmit.bind(this)}>
-        <div className="input-group">
-          <span className="input-group-addon" id="sizing-addon2">@</span>
-          <input type="text" className="form-control" ref="email" name="email" placeholder="Email" aria-describedby="sizing-addon2"/>
-        </div>
-        <br></br>
-        <div className="input-group">
-          <span className="input-group-addon" id="sizing-addon2">@</span>
-          <input type="text" className="form-control" ref="firstName" name="firstName" placeholder="Nombre" aria-describedby="sizing-addon2"/>
-        </div>
-        <br></br>
-        <div className="input-group">
-          <span className="input-group-addon" id="sizing-addon2">@</span>
-          <input type="text" className="form-control" ref="lastName" name="lastName" placeholder="Apellido" aria-describedby="sizing-addon2"/>
-        </div>
-        <br></br>
-        <div className="input-group">
-          <span className="input-group-addon" id="sizing-addon2">@</span>
-          <input type="text" className="form-control" ref="cellphoneNumber" name="cellphoneNumber" placeholder="Celular" aria-describedby="sizing-addon2"/>
-        </div>
-        <br></br>
-        <div className="input-group">
-          <span className="input-group-addon" id="sizing-addon2">@</span>
-          <input type="text" className="form-control" ref="age" name="age" placeholder="Edad" aria-describedby="sizing-addon2"/>
-        </div>
-        <br></br>
+      <div className="jumbotron">
         <div className="row">
-          <div className="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-offset-3 col-xs-6">
-            <div className="text-center">
-              <button className="btn btn-primary btn-md btn-block">informate!</button>
-            </div>
+          <div className="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
+            <form onSubmit={this.onSubmit.bind(this)}>
+              <div className="input-group">
+                <span className="input-group-addon" id="sizing-addon2"></span>
+                <input type="text" className="form-control" ref="email" name="email" placeholder="Correo Electronico" aria-describedby="sizing-addon2"/>
+              </div>
+              <br></br>
+              <div className="input-group">
+                <span className="input-group-addon" id="sizing-addon2"></span>
+                <input type="text" className="form-control" ref="firstName" name="firstName" placeholder="Nombre" aria-describedby="sizing-addon2"/>
+              </div>
+              <br></br>
+              <div className="input-group">
+                <span className="input-group-addon" id="sizing-addon2"></span>
+                <input type="text" className="form-control" ref="lastName" name="lastName" placeholder="Apellido" aria-describedby="sizing-addon2"/>
+              </div>
+              <br></br>
+              <div className="input-group">
+                <span className="input-group-addon" id="sizing-addon2"></span>
+                <input type="text" className="form-control" ref="cellphoneNumber" name="cellphoneNumber" placeholder="Celular" aria-describedby="sizing-addon2"/>
+              </div>
+              <br></br>
+              <div className="input-group">
+                <span className="input-group-addon" id="sizing-addon2"></span>
+                <input type="text" className="form-control" ref="age" name="age" placeholder="Edad" aria-describedby="sizing-addon2"/>
+              </div>
+              <br></br>
+              <div className="row">
+                <div className="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-offset-3 col-xs-6">
+                  <div className="text-center">
+                    <button className="btn btn-primary btn-md btn-block">¡informate!</button>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-      </form>
+      </div>
     );
   };
 };
--- imports/ui/FrontPage.js
+++ imports/ui/FrontPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class FrontPage extends React.Component {
   render() {
     return (
-      <div className="jumbotron">
+      <div className="jumbotron" id="front-page">
         <h1>Servagen</h1>
       </div>
     );
